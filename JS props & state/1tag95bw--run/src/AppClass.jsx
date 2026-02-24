6008312046200cc2f66cddd8b0e8abec96294507
AlphaMikey23 <eng22cse001@gmail.com>
2026-02-24 16:54:27 +0530

    Finished Prop but having doubts

--- JS props & state/1tag95bw--run/src/AppClass.jsx
+++ JS props & state/1tag95bw--run/src/AppClass.jsx
@@ -3,17 +3,18 @@ import "./App.css";
 
 export default class AppClass extends Component {
   render() {
-    // code here
+    const data = this.props.data || [];
 
     return (
       <div>
-        {/* 
-          TODO: 
-          1. Render the same HTML structure as App.jsx 
-          2. Access data using this.props.data 
-          3. Map through the data and render images 
-        */}
-        
+        <h1>Kalvium Gallery</h1>
+        <div className="row">
+          {data.map((image) => (
+            <div className="column" key={image.id}>
+              <img src={image.img} alt={`Gallery image ${image.img}`} />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
